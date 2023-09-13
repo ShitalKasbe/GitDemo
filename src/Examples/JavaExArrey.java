22efd8bd6affa0de56e28213d34104750c9c10c4
Girish Jorigal <Girish Jorigal@192.168.1.67>
2023-09-13 14:00:37 +0530

    first commet

---
+++ src/Examples/JavaExArrey.java
@@ -0,0 +1,22 @@
+package Examples;
+
+public class JavaExArrey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int f []=new int[9];
+        f[0]=6;
+        f[1]=7;
+        f[2]=5;
+        f[3]=8;
+        f[4]=9;
+        for (int i:f) {
+        	System.out.println(i);
+        }
+        
+        
+        }
+        
+	
+
+}
